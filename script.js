e2b15616bb66da61ec8ce2a5e9c305cecd749336
Javier Ruiz <javierruizjr15@gmail.com>
2021-02-20 17:15:43 -0800

    javas

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log(moment());
+
+
+var today = moment().format('MMMM Do YYYY, h:mm:ss a');
+var hour = moment().format('LT');
+
+console.log(hour)
+
+document.getElementById('currentDay').innerHTML =
+
+  `Today is: ${today}`;
+
+nineshade = () => {
+  console.log('test1')
+
+  if (document.getElementById(9) < hour) {
+
+    document.getElementById(9).classList.add('past');
+    console.log('test')
+  }
+}
+
+nineshade();
+
+//   .classList.add('past');
+// .classList.add('present');
+// .classList.add('future');
