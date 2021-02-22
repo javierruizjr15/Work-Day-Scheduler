afc6b2a440224841e26c6a5b1ab8aaaf2a77df45
Javier Ruiz <javierruizjr15@gmail.com>
2021-02-21 17:53:24 -0800

    javascript movin

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 var today = moment().format('MMMM Do YYYY, h:mm:ss a');
-var hour = moment().format('LT');
+var hour = moment().format('H mm');
 
 console.log(hour)
 
@@ -9,17 +9,66 @@ document.getElementById('currentDay').innerHTML =
 
   `Today is: ${today}`;
 
-nineshade = () => {
-  console.log('test1')
 
-  if (document.getElementById(9) < hour) {
+// past present future shade start++
+// 9 block start++
+nineshade = () => {
+
+  if (hour >= 10 & hour > 17) {
+
 
     document.getElementById(9).classList.add('past');
-    console.log('test')
   }
+
+
+
+  if (hour >= 9 && hour <= '9 59') {
+    document.getElementById(9).classList.add('present');
+  }
+
+
+
+  if (hour < 9 && hour < 17) {
+    document.getElementById(9).classList.add('future');
+  }
+
 }
 
+// call the fuction
 nineshade();
+// 9 block end--
+
+
+// 10 block start++
+tenshade = () => {
+
+  if (hour >= 11 & hour > 17) {
+
+
+    document.getElementById(9).classList.add('past');
+  }
+
+
+
+  if (hour >= 10 && hour <= '10 59') {
+    document.getElementById(9).classList.add('present');
+  }
+
+
+
+  if (hour < 10 && hour < 17) {
+    document.getElementById(9).classList.add('future');
+  }
+
+}
+// call the function
+tenshade();
+
+// 10 block end--
+
+
+// past present future shade end--
+
 
 //   .classList.add('past');
 // .classList.add('present');
